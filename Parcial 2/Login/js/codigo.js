55066f6481f88e7344577646b0c055174dc4b9bf
Lizandro Valdez <cotygut00gmail.com>
2021-04-28 01:18:58 -0500

    updated

--- Parcial 2/Login/js/codigo.js
+++ Parcial 2/Login/js/codigo.js
@@ -1,26 +1,28 @@
-(function(){
-    $(document).ready(function(){
-        $('.alt-form').click(function(){
-            $('.form-content').animate({
-                height: "toggle",
-                opacity: 'toggle'
-            }, 600);
-        });
+$(document).ready(function () {
+    
+//$('.container h1').html('cambio de texto');
+//$('.container h1:first').html('cambio de texto');
+//$('.container h1:last').html('cambio de texto');
 
-        let formRegistro = document.getElementsByName('form-input');
-        for (let i = 0; i < formRegistro.length; i++) {
-            formRegistro[i].addEventListener('blur', function(){
-                if (this.value.length >= 1) {
-                    this.nextElementSibling.classList.add('active');
-                    this.nextElementSibling.classList.remove('error');
-                } else if (this.value.length = " ") {
-                    this.nextElementSibling.classList.add('error');
-                    this.nextElementSibling.classList.remove('active');
-                } else {
-                    this.nextElementSibling.classList.remove('active');
-                }
-            })
-        }
 
-    })
-}())
+
+$('.btn-primary').click(function (e) { 
+    e.preventDefault();
+    var resultado = $('#mnj a')
+    var usuario=$('#usuario').val();
+    var pass=$('#password').val();
+   
+    console.log(usuario,pass)
+    if(usuario=='usuario' && pass=='1234')
+    {
+        resultado.html('Haga click aqui y ¡Gracias, pase usted!')
+        resultado.attr("href", "https://www.leagueofgraphs.com/es/summoner/lan/HexAztka");
+    }
+    else{
+        alert('No esta registrado');
+    }
+
+    
+});
+
+});
